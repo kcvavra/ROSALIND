34403b1a0c16aeab104c4534ba18359d6ae4dcad
KC Vavra <vavra.kc@gmail.com>
2015-12-01 20:18:36 -0600

    Added Strings and Lists problemset

---
+++ Village/StringsLists.py
@@ -0,0 +1,20 @@
+# This is the strings and lists
+# Sample input: HumptyDumptysatonawallHumptyDumptyhadagreatfallAlltheKingshorsesandalltheKingsmenCouldntputHumptyDumptyinhisplaceagain.
+#               22 27 97 102
+
+sSample = "HumptyDumptysatonawallHumptyDumptyhadagreatfallAlltheKingshorsesandalltheKingsmenCouldntputHumptyDumptyinhisplaceagain."
+aSample = 22
+bSample = 27+1
+cSample = 97
+dSample = 102+1
+
+print sSample[aSample:bSample], sSample[cSample:dSample]
+
+# Now for the real problem
+# Inputs copied from import file
+s = "CdqdczYxA8Wyl8SE9yUknTCJf0stCNOWjCokU3XhzNyPLBHNPvSRuticillaGtnqRleucorodiaIdW1N5SVDd3tcYduVL7DhqTNyi0yG0TLMggy96JMXK1TfdK2vxXn6JHTPX9jpGzY6DhQW7hOL16FZudsBbWYotMRE."
+a = 51
+b = 59 + 1
+c = 65
+d = 74 + 1
+print s[a:b], s[c:d]
