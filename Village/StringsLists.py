9b8a577067a88efc92a6b1cc722857a39c66fa73
KC Vavra <vavra.kc@gmail.com>
2016-01-05 22:51:34 -0600

    Modify StringsLists.py

--- Village/StringsLists.py
+++ Village/StringsLists.py
@@ -11,10 +11,11 @@ dSample = 102+1
 print sSample[aSample:bSample], sSample[cSample:dSample]
 
 # Now for the real problem
-# Inputs copied from import file
+# Inputs copied from import fileRuticilla leucorodia
 s = "CdqdczYxA8Wyl8SE9yUknTCJf0stCNOWjCokU3XhzNyPLBHNPvSRuticillaGtnqRleucorodiaIdW1N5SVDd3tcYduVL7DhqTNyi0yG0TLMggy96JMXK1TfdK2vxXn6JHTPX9jpGzY6DhQW7hOL16FZudsBbWYotMRE."
 a = 51
 b = 59 + 1
 c = 65
 d = 74 + 1
 print s[a:b], s[c:d]
+Ruticilla leucorodiaRuticilla leucorodia
